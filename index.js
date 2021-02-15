210d396b479d053498f8998820f306ff78615b59
JasminAtay <58908990+JasminAtay@users.noreply.github.com>
2021-02-15 20:35:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+let milliSecond = 0;
+let second = 0;
+let minute = 0;
+
+let displayMilliSec = 0;
+let displaySec = 0;
+let displayMinute = 0;
+
+
+let interval = null;
+
+
+let status = "stopped";
+
+function stopWatch(){
+
+    milliSecond++;
+    
+    if(milliSecond/100 ===1){
+        milliSecond = 0;
+        second++;
+        if(second/60 ===1){
+            second=0;
+            minute++;
+        }
+
+    }
+
+
+    
+    if(milliSecond < 10){
+        displayMilliSec = "0" + milliSecond.toString();
+    }else{
+        displayMilliSec = milliSecond;
+    }
+    if(second < 10){
+        displaySec = "0" + second.toString();
+    }else{
+        displaySec = second;
+    }
+    if(minute < 10){
+        displayMinute = "0" + minute.toString();
+    }else{
+        displayMinute = minute;
+    }
+
+     
+    document.getElementById("display").innerHTML = displayMinute + ":" + displaySec + ":" + displayMilliSec;
+
+
+
+}
+
+
+function startStop(){
+    if(status==="stopped"){
+
+        interval = window.setInterval(stopWatch,10);
+        document.getElementsByClassName("start-stop").innerHTML = "Stop";
+        status = "started";
+    }else {
+        window.clearInterval(interval);
+        document.getElementsByClassName("start-stop").innerHTML = "Start";
+        status = "stopped";
+    }
+
+}
+
+function reset(){
+    
+    window.clearInterval(interval);
+    milliSecond = 0;
+    second = 0;
+    minute = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementsByClassName("start-stop").innerHTML = "Start";
+}
+
